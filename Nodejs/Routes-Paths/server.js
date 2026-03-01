040d11c42f8c82ad3e8dede2712c78b965b10581
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 17:08:51 +0500

    Enhance error handling in getData function: improve robustness by adding checks for file existence and handling potential read errors gracefully.

--- Nodejs/Routes-Paths/server.js
+++ Nodejs/Routes-Paths/server.js
@@ -1,5 +1,6 @@
 import http from "node:http"
 import { serveStatic } from "./utils/serveStatic.js";
+import { handleGet } from "./handlers/handleGet.js";
 
 const PORT = 8000
 const __dirname = import.meta.dirname;
