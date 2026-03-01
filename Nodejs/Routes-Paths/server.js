0c21f4da6fa6a65e10e588853fb170fffa61d269
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 20:17:53 +0500

    Add POST request handling in server.js: integrate handlePost function for processing API requests, enhancing server capabilities.

--- Nodejs/Routes-Paths/server.js
+++ Nodejs/Routes-Paths/server.js
@@ -1,6 +1,7 @@
 import http from "node:http"
 import { serveStatic } from "./utils/serveStatic.js";
 import { handleGet } from "./handlers/handleGet.js";
+import { handlePost } from "./handlers/handlePost.js";
 
 const PORT = 8000
 const __dirname = import.meta.dirname;
@@ -9,6 +10,8 @@ const server = http.createServer(async (req, res) => {
     if (req.url.startsWith("/api")) {
         if (req.method === "GET") {
             return handleGet(res)
+        } else if (req.url === "POST") {
+            return handlePost(req, res)
         }
     } else if (!req.url.startsWith('/api')) {
         return await serveStatic(req, res, __dirname)
