ae5e6234e55d11968adc30e6e8b4fb29240f907d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 20:02:44 +0500

    improving

--- Nodejs/Routes-Paths/public/index.js
+++ Nodejs/Routes-Paths/public/index.js
@@ -1,38 +1,39 @@
-const cardContainer = document.querySelector(".cards-container")
-
 try {
     const data = await fetch("/api")
     const response = await data.json()
     renderCards(response)
-} catch (error) {
-    console.log(error);
+} catch (err) {
+    console.log(err)
 }
 
 function renderCards(cardsData) {
-    let cardHTML = ""
+    const container = document.querySelector(".cards-container")
+    let cardsHTML = ""
 
     cardsData.forEach((card, i) => {
-        cardHTML = `
-        <article class="sighting-card" aria-labelledby="sighting-title-${i}">
-            <p class="card-details">${card.timeStamp}, ${card.location}</p>
-            <h3 id="sighting-title-${i}">${card.title}</h3>
-            <div class="sighting-text-wrapper">
-                <p class="sighting-text">${card.text}</p>
-            </div>
-            <button class="read-more-btn" aria-expanded="false">Read in full</button>
-        </article>
-        `
-    });
-    cardContainer.innerHTML = cardHTML
+        cardsHTML += `
+  <article class="sighting-card" aria-labelledby="sighting-title-${i}">
+    <p class="card-details">${card.timeStamp}, ${card.location}</p>
+    <h3 id="sighting-title-${i}">${card.title}</h3>
+    <div class="sighting-text-wrapper">
+      <p class="sighting-text">${card.text}</p>
+    </div>
+    <button class="read-more-btn" aria-expanded="false">Read in full</button>
+  </article>
+    `
+    })
+
+    container.innerHTML = cardsHTML
 }
 
-cardContainer.addEventListener("click", (e) => {
-    if (!e.target.classList.contains("read-more-btn")) return;
+// handle card expand/collapse
+document.querySelector(".cards-container").addEventListener("click", (e) => {
+    if (!e.target.classList.contains("read-more-btn")) return
 
     const button = e.target
     const sightingCard = button.closest(".sighting-card")
-    const isExpanded = sightingCard.classList.toggle(".expanded")
+    const isExpanded = sightingCard.classList.toggle("expanded")
 
     button.setAttribute("aria-expanded", isExpanded ? "true" : "false")
-    button.textContent = isExpanded ? "Show Less" : "Read in full"
+    button.textContent = isExpanded ? "Show less" : "Read in full"
 })
