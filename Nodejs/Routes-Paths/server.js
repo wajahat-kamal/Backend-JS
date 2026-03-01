c92270dc9d404dc31207b938533172b948846ad1
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 16:52:34 +0500

    Add getData import and log statement in server.js: prepare for data retrieval functionality

--- Nodejs/Routes-Paths/server.js
+++ Nodejs/Routes-Paths/server.js
@@ -1,11 +1,15 @@
 import http from "node:http"
 import { serveStatic } from "./utils/serveStatic.js";
+import { getData } from "./utils/getData.js";
 
 const PORT = 8000
 const __dirname = import.meta.dirname;
 
+
 const server = http.createServer(async (req, res) => {
     // if (req.url === "/" && req.method === "GET") {
+    console.log(getData);
+
 
     await serveStatic(req, res, __dirname)
 
