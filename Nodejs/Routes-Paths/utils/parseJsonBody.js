0f5bae941ab21026698be92bf96cb58f8c8138b8
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 20:52:46 +0500

    Refactor parseJsonBody function to use async/await for reading request body: enhance error handling by throwing a descriptive error for invalid JSON formats.

--- Nodejs/Routes-Paths/utils/parseJsonBody.js
+++ Nodejs/Routes-Paths/utils/parseJsonBody.js
@@ -1,7 +1,14 @@
 export async function parseJsonBody(req) {
-    const body = ''
-    for (const chunk of req) {
-        body += chunk
+    let body = '';
+
+    try {
+        for await (const chunk of req) {
+            body += chunk;
+        }
+
+        return JSON.parse(body);
+
+    } catch (err) {
+        throw new Error(`Invalid JSON format: ${err}`);
     }
-    return body
 }
