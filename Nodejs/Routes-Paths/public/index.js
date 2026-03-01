62b9b9ca1deedd893bd2f052059155ec0b3476ea
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 20:07:21 +0500

    Refactor handleGet function to use async/await for data retrieval: improve response handling by ensuring data is fetched before sending the response. Update upload-sighting.js to enhance form submission logic and error handling. Comment out background image in index.css for future reference.

--- Nodejs/Routes-Paths/public/index.js
+++ Nodejs/Routes-Paths/public/index.js
@@ -2,38 +2,36 @@ try {
     const data = await fetch("/api")
     const response = await data.json()
     renderCards(response)
-} catch (err) {
-    console.log(err)
+} catch (error) {
+    console.log(error);
 }
 
 function renderCards(cardsData) {
-    const container = document.querySelector(".cards-container")
-    let cardsHTML = ""
+    const cardContainer = document.querySelector(".cards-container")
+    let cardHTML = ""
 
     cardsData.forEach((card, i) => {
-        cardsHTML += `
-  <article class="sighting-card" aria-labelledby="sighting-title-${i}">
-    <p class="card-details">${card.timeStamp}, ${card.location}</p>
-    <h3 id="sighting-title-${i}">${card.title}</h3>
-    <div class="sighting-text-wrapper">
-      <p class="sighting-text">${card.text}</p>
-    </div>
-    <button class="read-more-btn" aria-expanded="false">Read in full</button>
-  </article>
-    `
-    })
-
-    container.innerHTML = cardsHTML
+        cardHTML += `
+        <article class="sighting-card" aria-labelledby="sighting-title-${i}">
+            <p class="card-details">${card.timeStamp}, ${card.location}</p>
+            <h3 id="sighting-title-${i}">${card.title}</h3>
+            <div class="sighting-text-wrapper">
+                <p class="sighting-text">${card.text}</p>
+            </div>
+            <button class="read-more-btn" aria-expanded="false">Read in full</button>
+        </article>
+        `
+    });
+    cardContainer.innerHTML = cardHTML
 }
 
-// handle card expand/collapse
 document.querySelector(".cards-container").addEventListener("click", (e) => {
-    if (!e.target.classList.contains("read-more-btn")) return
+    if (!e.target.classList.contains("read-more-btn")) return;
 
     const button = e.target
     const sightingCard = button.closest(".sighting-card")
-    const isExpanded = sightingCard.classList.toggle("expanded")
+    const isExpanded = sightingCard.classList.toggle(".expanded")
 
     button.setAttribute("aria-expanded", isExpanded ? "true" : "false")
-    button.textContent = isExpanded ? "Show less" : "Read in full"
+    button.textContent = isExpanded ? "Show Less" : "Read in full"
 })
