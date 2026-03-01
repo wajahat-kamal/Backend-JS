67cafa0370dafb5baafe97dddccde3d1afe9d50c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 16:40:02 +0500

    Refactor getData function to improve file reading logic: update path construction for data.json and parse the data before returning it.

--- Nodejs/Routes-Paths/utils/getData.js
+++ Nodejs/Routes-Paths/utils/getData.js
@@ -1,9 +1,10 @@
 import fs from 'node:fs'
+import path from 'node:path';
 
 export async function getData() {
     try {
-        const data = await fs.readFile("data.json")
-        console.log(data);
+        const data = await fs.readFile(path.join(import.meta.dirname, "data", "data.json"))
+        console.log(data.parse());
 
         return data
     } catch (error) {
@@ -11,3 +12,4 @@ export async function getData() {
     }
 
 }
+getData()
