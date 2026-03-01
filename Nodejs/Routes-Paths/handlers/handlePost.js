611e296a6bdeb90074ecfbf9199a03b2b26f4433
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 20:26:14 +0500

    Implement form validation in handlePost function: add checks for required fields and return error response if validation fails. Update server.js to comment out handlePost call for future implementation. Add console logging in getData function for debugging purposes.

--- Nodejs/Routes-Paths/handlers/handlePost.js
+++ Nodejs/Routes-Paths/handlers/handlePost.js
@@ -1,3 +1,11 @@
+import { sendResponse } from "../utils/sendResponse.js";
+
 export async function handlePost(req, res) {
+    const {title, text, timpStamp, location} = req.body;
+
+    if (!title || !text || !timpStamp || !location) {
+        return sendResponse(res, 404, "application/json", "All fields are required!")
+    }
+
     
 }
--- Nodejs/Routes-Paths/utils/getData.js
+++ Nodejs/Routes-Paths/utils/getData.js
@@ -5,6 +5,9 @@ export async function getData() {
     try {
         const data = await fs.readFile(path.join("data", "data.json"), "utf8")
         const result = JSON.parse(data)
+        console.log(result);
+        
+        
         return result
     } catch (error) {
         console.log(error);
