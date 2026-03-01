1789d6ddfbce367dfe898f0f7ea33ddb55deb792
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-01 16:38:11 +0500

    Implement getData function to read and return data from data.json: add error handling for file read operations and log errors to the console.

--- Nodejs/Routes-Paths/utils/getData.js
+++ Nodejs/Routes-Paths/utils/getData.js
@@ -1,3 +1,13 @@
-export async function  getData() {
-    
+import fs from 'node:fs'
+
+export async function getData() {
+    try {
+        const data = await fs.readFile("data.json")
+        console.log(data);
+
+        return data
+    } catch (error) {
+        console.log(error);
+    }
+
 }
