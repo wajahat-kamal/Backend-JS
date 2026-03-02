ad9ddf91a74c6f4ce00e51db5dc99a13ddfc95e9
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-02 06:10:07 +0500

    Refactor server.js to improve API request handling: update POST request logic, ensure proper method checks, and enhance logging. Modify handleGet and handlePost functions for consistency and clarity in response handling.

--- Nodejs/Routes-Paths/handlers/handlers.js
+++ Nodejs/Routes-Paths/handlers/handlers.js
@@ -1,20 +1,13 @@
-import { getData } from "../utils/getData.js";
-import { parseJsonBody } from "../utils/parseJsonBody.js";
-import { sendResponse } from "../utils/sendResponse.js";
-
+import { getData } from '../utils/getData.js'
+import { parseJsonBody } from '../utils/parseJsonBody.js'
+import { sendResponse } from '../utils/sendResponse.js'
 export async function handleGet(res) {
     const data = await getData()
-    const jsonData = JSON.stringify(data)
-    sendResponse(res, 200, "application/json", jsonData)
+    const content = JSON.stringify(data)
+    sendResponse(res, 200, 'application/json', content)
 }
 
 export async function handlePost(req, res) {
-    // const {title, text, timpStamp, location} = req.body;
-
-    // if (!title || !text || !timpStamp || !location) {
-    //     return sendResponse(res, 404, "application/json", "All fields are required!")
-    // }
-    const rawBody = parseJsonBody(req)
-    console.log(rawBody);
-
+    const rawBody = await parseJsonBody(req)
+    console.log(rawBody)
 }
--- Nodejs/Routes-Paths/server.js
+++ Nodejs/Routes-Paths/server.js
@@ -1,21 +1,24 @@
-import http from "node:http"
-import { serveStatic } from "./utils/serveStatic.js";
-import { handleGet } from "./handlers/handlers.js";
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
+import { handleGet, handlePost } from './handlers/handlers.js'
 
 const PORT = 8000
-const __dirname = import.meta.dirname;
+
+const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
-    if (req.url.startsWith("/api")) {
-        if (req.method === "GET") {
-            return handleGet(res)
 
-        } else if (req.url === "POST") {
-            return handlePost(req, res)
+    if (req.url === '/api') {
+        if (req.method === 'GET') {
+            return await handleGet(res)
         }
-    } else if (!req.url.startsWith('/api')) {
+        else if (req.method === 'POST') {
+            handlePost(req, res)
+        }
+    }
+    else if (!req.url.startsWith('/api')) {
         return await serveStatic(req, res, __dirname)
     }
 })
 
-server.listen(PORT, () => console.log(`Server runing on port: ${PORT}`))
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
