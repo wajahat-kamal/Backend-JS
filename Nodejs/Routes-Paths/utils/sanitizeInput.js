e7f5b5234774f25eda6b2c48656eaa2859423d37
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-02 12:52:04 +0500

    Add sanitizeInput function to sanitize input data using the sanitize-html library. This implementation ensures that only allowed HTML tags are preserved, enhancing data integrity and security.

--- Nodejs/Routes-Paths/utils/sanitizeInput.js
+++ Nodejs/Routes-Paths/utils/sanitizeInput.js
@@ -1,3 +1,14 @@
-export function sanitizeInput(data) {
+import sanitize from "sanitize-html"
 
+export function sanitizeInput(data) {
+    const sanitizeData = {}
+
+    for (const [key, value] of Object.entries(data)) {
+        if (typeof value === "string") {
+            sanitizeData[key] = sanitize(value, { allowedTags: ["b"], allowedAttributes: {} })
+        } else {
+            sanitizeData[key] = value
+        }
+    }
+    return sanitizeData
 }
