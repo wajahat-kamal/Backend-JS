c8865546297ec97aa0d5c29bad0ab9c2f83058f0
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-02 17:21:54 +0500

    Refactor server routing logic to prioritize news API requests, improving clarity and organization. This change ensures that news-related requests are handled first, while maintaining existing functionality for other API endpoints.

--- Nodejs/Routes-Paths/server.js
+++ Nodejs/Routes-Paths/server.js
@@ -6,16 +6,14 @@ const PORT = 8000
 const __dirname = import.meta.dirname;
 
 const server = http.createServer(async (req, res) => {
-    if (req.url.startsWith("/api")) {
+    if (req.url.startsWith("/api/news")) {
+        return await handleNews(req, res)
+    } else if (req.url.startsWith("/api")) {
         if (req.method === "GET") {
             return handleGet(res)
-
         } else if (req.method === "POST") {
             return handlePost(req, res)
         }
-    } else if (req.url.startsWith("/api/news")) {
-        return await handleNews(req, res)
-
     } else if (!req.url.startsWith('/api')) {
         return await serveStatic(req, res, __dirname)
     }
