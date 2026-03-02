306337207b0f76794acd86e3051889066559f1eb
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-02 22:28:47 +0500

    Update CJS-ESM.js to provide a detailed comparison of CommonJS and ESModules, highlighting syntax differences, loading behavior, and module type requirements.

---
+++ Nodejs/ESM-CJS.js
@@ -0,0 +1,16 @@
+// What is the difference between ESModules and CommonJS
+
+/*
+1. zyada tar syntax ka farq hai sirf commonjs me hum require aur module.export use karte aur esmodules me import aur export ka use karte hai. 
+
+2. commonjs synchronous hota hai matlab ik ke baad load hota hai aur esmodules asynchronous hosakta hai jo apps me performance behtar kar sakta hai.
+
+3. commonjs default hota hai. aur esmodules ko use karne ke liye ya toh file extension ko .mjs karna hota hai ya package.json file me "type": "module" karna hota hai
+
+*/
+
+// ESModules:
+// export and import
+
+// CommonJS:
+// module.export and require()
