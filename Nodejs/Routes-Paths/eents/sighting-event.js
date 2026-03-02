5a39537e39774908abd63d27f8938faaeae40800
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-02 15:58:47 +0500

    Add sighting event handling by exporting sightingEvents and setting up an event listener for "sighting-added" to trigger alerts. This enhances the event-driven architecture for managing sightings.

--- Nodejs/Routes-Paths/eents/sighting-event.js
+++ Nodejs/Routes-Paths/eents/sighting-event.js
@@ -1,3 +1,6 @@
 import { EventEmitter } from 'node:events'
 import { createAlert } from '../utils/createAlert.js'
 
+export const sightingEvents = new EventEmitter()
+
+emitter.on("sighting-added", createAlert)
