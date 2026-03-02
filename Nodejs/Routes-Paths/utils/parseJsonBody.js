becade73e5a054a6cb5ddc482464fc5b63979ccb
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-02 05:54:10 +0500

    Refactor parseJsonBody function to remove unnecessary code: streamline the body reading process by eliminating redundant lines while maintaining async/await functionality for JSON parsing.

--- Nodejs/Routes-Paths/utils/parseJsonBody.js
+++ Nodejs/Routes-Paths/utils/parseJsonBody.js
@@ -1,13 +1,10 @@
 export async function parseJsonBody(req) {
     let body = '';
-
+    for await (const chunk of req) {
+        body += chunk;
+    }
     try {
-        for await (const chunk of req) {
-            body += chunk;
-        }
-
         return JSON.parse(body);
-
     } catch (err) {
         throw new Error(`Invalid JSON format: ${err}`);
     }
