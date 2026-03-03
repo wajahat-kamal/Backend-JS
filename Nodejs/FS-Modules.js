4357c3a48a24a16ccd9bdf9de7c1ad19e15eea39
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-03 11:32:13 +0500

    Add file handling examples in FS-Modules.js using fs/promises. This update includes creating and reading a file, enhancing the demonstration of modern Node.js file system operations.

--- Nodejs/FS-Modules.js
+++ Nodejs/FS-Modules.js
@@ -10,3 +10,13 @@ unlink()	  File delete
 mkdir()	      Folder create
 readdir()	  Folder ke files read
 */
+
+// Modern + Recommended
+import fs from "node:fs/promises"
+
+// create file
+await fs.writeFile("test.txt", "Hi, Iam Nodejs!")
+
+// read file
+const content= await fs.readFile("test.txt", "utf8")
+console.log(content);
