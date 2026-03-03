fb99e8a82d6651f2ef6194f956f102e21b1ebd7a
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-03 16:23:38 +0500

    Add JWT authentication token generation in Authentication-JWT.js. This update introduces token creation upon successful login, enhancing the server's authentication capabilities and allowing for secure user sessions.

--- Nodejs/Authentication-JWT.js
+++ Nodejs/Authentication-JWT.js
@@ -3,10 +3,13 @@
 
 
 import http from "node:http"
+import jwt from "jsonwebtoken"
 
 const server = http.createServer((req, res) => {
     const loginSuccess = true
-    if(loginSuccess) {
-        
+    if (loginSuccess) {
+        const token = jwt.sign({ userId: 1 }, "secretKey", { expiresIn: "1h" })
+        res.end({ token })
     }
 })
+server.listen(5000, () => console.log("Server runing on port 5000"))
