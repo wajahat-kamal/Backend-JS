99c10e55ddf5718c53706b65620fa0dec7ac3a28
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-03 12:10:35 +0500

    Add initial explanation of Promises in Promises-AsyncAwait.js, detailing their states and purpose. This introduction enhances understanding of asynchronous programming in Node.js.

---
+++ Nodejs/Promises-AsyncAwait.js
@@ -0,0 +1,12 @@
+// What is promises
+
+/*
+Promise ek object hota hai jo future me milne wali value ko represent karta hai.
+matlab abhi result avalible nahi lekin future me mile ga.
+
+promise ki 3 states hoti hai:
+ pending: abhi kaam hora hai.
+ resolve: kaam complete hogaya.
+ rejected: error agaya
+*/
+
