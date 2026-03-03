e13850c9b646e9327a86d9d5c5cd78406305ae91
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-03 16:20:24 +0500

    Add initial HTTP server setup in Authentication-JWT.js for user login handling. This update introduces a basic server structure and prepares for future authentication logic.

--- Nodejs/Authentication-JWT.js
+++ Nodejs/Authentication-JWT.js
@@ -1,2 +1,12 @@
 // Authentication: User ki identity verify karna (login karna)
 // JWT: jwt ik secure token deta hai jo server user ko login hone baad deta hai
+
+
+import http from "node:http"
+
+const server = http.createServer((req, res) => {
+    const loginSuccess = true
+    if(loginSuccess) {
+        
+    }
+})
