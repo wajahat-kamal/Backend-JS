91c0a0e24b95914bc6bfd5335a3fd72f24c41140
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-03 11:28:52 +0500

    Refactor CJS-ESM.js to enhance the comparison of CommonJS and ESModules by adding examples and clarifying module loading behavior. This update improves the overall understanding of module type requirements and syntax differences.

---
+++ Nodejs/FS-Modules.js
@@ -0,0 +1,12 @@
+// What is fs modules
+/*
+fs ka matlab hai File System module.
+Ye Node.js ka built-in module hai jo files aur folders ke sath kaam karne ke liye use hota hai.
+
+readFile()    File read
+writeFile()	  File create/overwrite
+appendFile()  Data add karna
+unlink()	  File delete
+mkdir()	      Folder create
+readdir()	  Folder ke files read
+*/
