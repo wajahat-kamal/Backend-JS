5720f3d9321489d23c85b2b2e7ef32aa1b80ec70
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-04 15:19:04 +0500

    Remove script.js and style.css files to streamline the project and eliminate unused code. This cleanup enhances maintainability and reduces clutter in the views directory.

--- Expressjs/views/script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("greetBtn")
-const welcome = document.getElementById("welcome")
-
-button.addEventListener("click", () => {
-    welcome.textContent = "Hey, Wajahat! 👋";
-})
