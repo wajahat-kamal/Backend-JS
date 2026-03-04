c205a0efee3c16f144e07448bc63b8dea99dd1c6
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-04 10:29:34 +0500

    Add built-in middleware to server.js for JSON and URL-encoded data parsing. This update enhances the server's ability to handle incoming request data, improving overall functionality.

--- Expressjs/server.js
+++ Expressjs/server.js
@@ -3,6 +3,11 @@ import express from "express";
 const app = express()
 const port = 5000;
 
+// Built in Middlewares
+app.use(express.json()) // JSON body ko parse karta hai
+app.use(express.urlencoded()) // Form data (HTML se anne wala data) parse karta hai
+// app.use(express.static()) // static files serve karta hai (HTML, CSS, JS, Images) 
+
 // Middleware
 app.use((req, res, next) => {
     console.log(`URL: ${req.url} Method: ${req.method}`)
