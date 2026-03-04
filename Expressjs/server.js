492a14399995c22ea2bc6392d5606902c9046de1
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-04 10:03:54 +0500

    Refactor server.js to streamline server listening logic by consolidating the console log statement into a single line. This change improves code readability while maintaining functionality.

--- Expressjs/server.js
+++ Expressjs/server.js
@@ -7,6 +7,4 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(port, () => {
-    console.log(`Server runing on port ${port}`)
-  })
+app.listen(port, () => console.log(`Server runing on port ${port}`))
