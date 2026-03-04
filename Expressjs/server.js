e7a59b94e69e57ad98bd12d1a493ca80a9da59cc
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-04 10:23:02 +0500

    Add middleware to server.js for logging request details. This update introduces a logging middleware that captures and displays the request URL and method, enhancing debugging and monitoring capabilities.

--- Expressjs/server.js
+++ Expressjs/server.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express()
 const port = 5000;
 
+// Middleware
+app.use((req, res, next) => {
+    console.log(req.url, req.method)
+    next();
+})
+
 // Route Params
 app.get("/name/:name", (req, res) => {
     res.send(req.params.name)
