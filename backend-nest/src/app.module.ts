978d6fee78fd06ec23e3b957d7c97c3554139ba4
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-16 11:14:32 +0500

    Add ProductsService to AppModule for product management functionality

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
+import { ProductsService } from './products/products.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UserController],
-  providers: [AppService],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
---
+++ backend-nest/src/products/products.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductsService {
+    private products = [
+        { id: 1, name: "Mobile", price: 40000 },
+        { id: 2, name: "Laptop", price: 80000 },
+        { id: 3, name: "Tablet", price: 30000 },
+    ];
+    getAllProducts() {
+        return this.products
+    }
+}
