06e5efa57efe1d52979caa1aa99c79fb61752ea5
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-16 11:47:28 +0500

    Refactor AppModule to integrate ProductModule, removing UserController and its tests for streamlined user management

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { ProductsService } from './products/products.service';
-import { ProductController } from './product/product.controller';
-import { ProductService } from './product/product.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController, ProductController],
-  providers: [AppService, ProductsService, ProductService],
+  imports: [ProductModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
