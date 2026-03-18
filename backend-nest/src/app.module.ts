995977d43e1be44849663319a3ca77da9785d4aa
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-18 12:44:42 +0500

    Integrate StudentModule into AppModule for enhanced student management functionality

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, StudentModule],
   controllers: [AppController],
   providers: [AppService],
 })
