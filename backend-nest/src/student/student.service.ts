abdef3f27bcf91abc80e7c8d823ce281aa641d97
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-18 13:09:23 +0500

    Refactor student array naming in StudentService for clarity and add deleteStudent method for removing students by ID

--- backend-nest/src/student/student.service.ts
+++ backend-nest/src/student/student.service.ts
@@ -4,7 +4,7 @@ type student = { name: string, age: number }
 
 @Injectable()
 export class StudentService {
-    private student = [
+    private students = [
         { id: 1, name: "Wajahat", age: 16 },
         { id: 2, name: "Kamal", age: 17 },
         { id: 3, name: "Khan", age: 18 },
@@ -12,11 +12,11 @@ export class StudentService {
 
     // GET
     getAllStudent() {
-        return this.student
+        return this.students
     }
 
     getStudentById(id: number) {
-        const student = this.student.find((st) => st.id === id)
+        const student = this.students.find((st) => st.id === id)
         if (!student) throw new NotFoundException("Student not found")
         return student
     }
@@ -27,15 +27,15 @@ export class StudentService {
             id: Date.now(),
             ...data
         }
-        this.student.push(newStudent)
+        this.students.push(newStudent)
         return newStudent;
     }
 
     // PUT
     updateStudentAllData(id: number, data: student) {
-        const index = this.student.findIndex((st) => st.id === id)
+        const index = this.students.findIndex((st) => st.id === id)
         if (index === -1) throw new NotFoundException("Student not found")
-        const updateStudent = this.student[index] = { id, ...data }
+        const updateStudent = this.students[index] = { id, ...data }
         return updateStudent
     }
 
@@ -44,5 +44,11 @@ export class StudentService {
         const student = this.getStudentById(id)
         Object.assign(student, data)
         return student
+    }
+
+    // DELETE
+    deleteStudent(id: number) {
+        const student = this.students.findIndex((s) => s.id === id)
+        
     }
 }
