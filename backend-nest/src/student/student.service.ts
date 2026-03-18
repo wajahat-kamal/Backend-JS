72e7936afb6dc3eed42d2bbb5759c5a2731f06d3
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-18 12:47:18 +0500

    Add student management methods to StudentService for retrieving all students and individual student by ID

--- backend-nest/src/student/student.service.ts
+++ backend-nest/src/student/student.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    private student = [
+        {id: 1, name: "Wajahat", age: 16},
+        {id: 2, name: "Kamal", age: 17},
+        {id: 3, name: "Khan", age: 18},
+    ]
+
+    getAllStudent(){
+        return this.student
+    }
+
+    getStudentById(id: number){
+        return this.student.find((st) => st.id === id)
+    }
+
+    
+}
