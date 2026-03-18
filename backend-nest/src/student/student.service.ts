1569d288ed9552d09c053a8231d8bdbb679f2b2d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-18 12:48:40 +0500

    Enhance getStudentById method in StudentService to throw an error for non-existent students and format student data for consistency

--- backend-nest/src/student/student.service.ts
+++ backend-nest/src/student/student.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class StudentService {
     private student = [
-        {id: 1, name: "Wajahat", age: 16},
-        {id: 2, name: "Kamal", age: 17},
-        {id: 3, name: "Khan", age: 18},
+        { id: 1, name: "Wajahat", age: 16 },
+        { id: 2, name: "Kamal", age: 17 },
+        { id: 3, name: "Khan", age: 18 },
     ]
 
-    getAllStudent(){
+    getAllStudent() {
         return this.student
     }
 
-    getStudentById(id: number){
-        return this.student.find((st) => st.id === id)
+    getStudentById(id: number) {
+        const student = this.student.find((st) => st.id === id)
+        if (student) {
+            return student
+        } else {
+            throw new NotFoundException
+        }
     }
 
-    
+
 }
