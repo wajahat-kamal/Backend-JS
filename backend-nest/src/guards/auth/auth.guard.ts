b642e52a528bf6420a45546b59ac6f86c3147037
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-21 14:05:22 +0500

    Implement authorization check in AuthGuard to validate bearer token for access control

--- backend-nest/src/guards/auth/auth.guard.ts
+++ backend-nest/src/guards/auth/auth.guard.ts
@@ -3,9 +3,9 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest()
+    const authHeader = request.headers['authorization'];
+    return authHeader === "Bearer my-secret-token"
   }
 }
