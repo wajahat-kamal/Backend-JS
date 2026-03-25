27d75dd9064ab3ab968bf4768fb9f2605f694bac
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-25 13:29:01 +0500

    Refactor Prisma integration to use @prisma/adapter-pg for PostgreSQL support. Update schema to enforce unique email constraint on User model. Modify package.json and package-lock.json to reflect new dependencies and remove mariadb references.

--- Prisma-mysql/index.ts
+++ Prisma-mysql/index.ts
@@ -1,17 +1,22 @@
-import { PrismaClient } from './generated/prisma'
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "./generated/prisma/client.ts"
 
-const prisma = new PrismaClient()
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+})
+
+const prisma = new PrismaClient({ adapter })
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: "Wajahat Kamal",
-      email: "wk@ex.com"
-    }
-  })
-  console.log(user)
+    const user = await prisma.user.create({
+        data: {
+            name: "Wajahat Kamal",
+            email: "wk@ex.com"
+        }
+    })
+    console.log(user)
 }
 
 main()
-  .catch(console.error)
-  .finally(() => prisma.$disconnect())
+    .catch(console.error)
+    .finally(() => prisma.$disconnect())
