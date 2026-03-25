2e522ff8909d88b4b4cca55a34a55fd028268336
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-25 13:35:09 +0500

    Fix import path for PrismaClient in index.ts to remove file extension, ensuring compatibility with TypeScript module resolution.

--- Prisma-mysql/index.ts
+++ Prisma-mysql/index.ts
@@ -1,22 +1,13 @@
-import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "./generated/prisma/client.ts"
+// async function main() {
+//     const user = await prisma.user.create({
+//         data: {
+//             name: "Wajahat Kamal",
+//             email: "wk@ex.com"
+//         }
+//     })
+//     console.log(user)
+// }
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL
-})
-
-const prisma = new PrismaClient({ adapter })
-
-async function main() {
-    const user = await prisma.user.create({
-        data: {
-            name: "Wajahat Kamal",
-            email: "wk@ex.com"
-        }
-    })
-    console.log(user)
-}
-
-main()
-    .catch(console.error)
-    .finally(() => prisma.$disconnect())
+// main()
+//     .catch(console.error)
+//     .finally(() => prisma.$disconnect())
