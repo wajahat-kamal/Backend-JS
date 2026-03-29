04a1d27d9deb0ec24c76be5aec0780783cc9a1b1
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-29 08:53:39 +0500

    Add Users module, controller, and service to implement user management functionality with a GET endpoint for retrieving all users.

--- nestjs/src/users/users.service.ts
+++ nestjs/src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+type user = {id: number, name: string, age: number}
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: user[] = [
+        { id: 1, name: "wajahat", age: 16 },
+        { id: 2, name: "kamal", age: 15 },
+        { id: 3, name: "khan", age: 14 },
+    ]
+
+    getAllUsers(){
+        return this.users
+    }
+}
